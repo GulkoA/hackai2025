a29b1d07ec79b9af327af6522d24fcbde3edbea0
Quinton Holmes <quintonrholmes30@gmail.com>
2025-02-22 17:33:20 -0500

    Animated character

--- UnlonelyIsland/Assets/Scripts/DebugClickToMove.cs
+++ UnlonelyIsland/Assets/Scripts/DebugClickToMove.cs
@@ -3,55 +3,33 @@ using UnityEngine.AI;
 
 public class DebugClickToMove : MonoBehaviour
 {
-    //Used for choices 1, 2, 3 in pathfinding.
-    private Vector3 housingArea;
-    private Vector3 market;
-    private Vector3 dock;
-
     private NavMeshAgent agent;
+    [SerializeField] private Animator animator;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
-        //1, 2, 3, for housing, shopping, hill options to raycast to.
-        housingArea = new Vector3(407.20f, 248.40f, 0.00f);
-        market = new Vector3(536.00f, 370.80f, 0.00f);
-        dock = new Vector3(836.00f, 382.00f, 0.00f);
     }
 
     void Update()
     {
-        Ray ray;
-        RaycastHit hit;
-
-
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetMouseButtonDown(0)) // Left click
         {
-            ray = Camera.main.ScreenPointToRay(housingArea);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
             if (Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
+                animator.SetTrigger("tWalk");
             }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
         {
-            ray = Camera.main.ScreenPointToRay(market);
-            if (Physics.Raycast(ray, out hit))
+            if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
             {
-                agent.SetDestination(hit.point);
+                animator.SetTrigger("tIdle");
             }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ray = Camera.main.ScreenPointToRay(dock);
-            if (Physics.Raycast(ray, out hit))
-            {
-                agent.SetDestination(hit.point);
-            }
-        }
-
-
     }
 }
