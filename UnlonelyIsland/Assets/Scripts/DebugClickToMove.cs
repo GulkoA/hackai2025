5435345cde531aa89ca4747cd0b19ab5027fe88a
M3gumii <144289754+M3gumii@users.noreply.github.com>
2025-02-22 16:36:46 -0500

    Fixed raycasting to 3 locations.

--- UnlonelyIsland/Assets/Scripts/DebugClickToMove.cs
+++ UnlonelyIsland/Assets/Scripts/DebugClickToMove.cs
@@ -4,42 +4,53 @@ using UnityEngine.AI;
 public class DebugClickToMove : MonoBehaviour
 {
     //Used for choices 1, 2, 3 in pathfinding.
-    private Vector3 housingArea = new Vector3();
-    private Vector3 shoppingArea = new Vector3();
-    private Vector3 mountainArea = new Vector3();
+    private Vector3 housingArea;
+    private Vector3 market;
+    private Vector3 dock;
 
     private NavMeshAgent agent;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        //1, 2, 3, for housing, shopping, hill options to raycast to.
+        housingArea = new Vector3(4.15f, 4.28f, -8.94f);
+        market = new Vector3(-17.50f, 8.09f, -9.73f);
+        dock = new Vector3(-46.64f, 3.33f, -11.94f);
     }
 
     void Update()
     {
-        //change this to a 1, 2, 3, for housing, shopping, hill.
-        //store each as a location vector3.
-        out hit = null;
+        Ray ray;
+        RaycastHit hit;
 
-        switch (Input.getMouseButton())
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            case 1: //go to housing
-                Ray ray = Camera.main.ScreenPointToRay(housingArea);
-                Physics.Raycast(ray, out hit);
-                break;
-            case 2: //go to shopping area
-                Ray ray = Camera.main.ScreenPointToRay(shoppingArea);
-                Physics.Raycast(ray, out hit);
-                break;
-            case 3: //go to the mountain
-                Ray ray = Camera.main.ScreenPointToRay(mountainArea);
-                Physics.Raycast(ray, out hit);
-                break;
-            default:
-                break;
+            ray = Camera.main.ScreenPointToRay(housingArea);
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+                Debug.Log("Hit! "+ hit.point);
+            }
         }
-        if (hit != null) {
-            agent.SetDestination(hit.point);
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ray = Camera.main.ScreenPointToRay(market);
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ray = Camera.main.ScreenPointToRay(dock);
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        
     }
 }
