bb44f887c15ab05cf643c9bf5b3ea938ecd7fd4b
Alex Gulko <alexgulko99@gmail.com>
2025-02-23 04:20:58 -0500

    humanoid_manager added

--- UnlonelyIsland/python/humanoid_manager.py
+++ UnlonelyIsland/python/humanoid_manager.py
@@ -1,4 +1,4 @@
-from humanoid import Humanoid
+from humanoid import Humanoid, Context, Inventory
 
 class HumanoidManager():
   def __init__(self):
@@ -6,4 +6,14 @@ class HumanoidManager():
     self.humanoids: dict[int, Humanoid] = {}
 
 
+  def prompt_agent_action(self, id, ctx):
+    context = Context.model_validate_json(ctx)
+    return self.humanoids[id].prompt_agent_action(context)
+  
+  def prompt_agent_conversation(self, partner_id: int, phrase: str, ctx: dict, trade_offer: dict, trade_receive: dict):
+    partner_name = self.humanoids[partner_id].name
+    context = Context.model_validate_json(ctx)
+    offer = Inventory.model_validate_json(trade_offer)
+    receive = Inventory.model_validate_json(trade_receive)
+    return self.humanoids[id].prompt_agent_conversation(partner_name, phrase, context, offer, receive)
   
